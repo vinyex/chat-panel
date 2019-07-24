9590268f1981cad29c0209eadeac9ad4c3611291
vinyex <irvinepramudya@gmail.com>
2019-07-24 17:42:51 +0700

    Update Seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin Virtual',
+            'username' => 'admin',
+            'email' => 'admin@mail.com',
+            'password' => bcrypt('123456'),
+        ]);
+    }
+}
